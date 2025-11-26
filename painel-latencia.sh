e08240dcbaea973067bd8e5376e5dadc3e0960cd
FeraAlpha <syllvaleandro7172@gmail.com>
2025-11-26 04:04:54 -0300

    Update painel-latencia.sh

--- painel-latencia.sh
+++ painel-latencia.sh
@@ -1,7 +1,6 @@
 #!/system/bin/sh
 MODDIR=${0%/*}
 
-# Carregar módulos
 source "$MODDIR/core/update.sh"
 source "$MODDIR/core/login.sh"
 source "$MODDIR/core/tweaks.sh"
@@ -9,8 +8,5 @@ source "$MODDIR/core/spoof.sh"
 source "$MODDIR/core/menu.sh"
 source "$MODDIR/core/boot.sh"
 
-# Verificar atualização automática
 check_update
-
-# Iniciar login → painel
 login_flow
