1f0a8b85c9b529e51fd53995ba858ccdae5738bb
FeraAlpha <syllvaleandro7172@gmail.com>
2025-11-26 04:24:01 -0300

    Update painel.sh

--- painel.sh
+++ painel.sh
@@ -1,1 +1,12 @@
+#!/system/bin/sh
+MODDIR=${0%/*}
 
+source "$MODDIR/core/update.sh"
+source "$MODDIR/core/login.sh"
+source "$MODDIR/core/tweaks.sh"
+source "$MODDIR/core/spoof.sh"
+source "$MODDIR/core/menu.sh"
+source "$MODDIR/core/boot.sh"
+
+check_update
+login_flow
